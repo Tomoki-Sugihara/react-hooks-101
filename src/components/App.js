4c48b9df7b106942d414a6bafd9bb21eb9464a21
sugihara10969 <sugihara.10969@gmail.com>
2020-07-11 17:51:42 +0900

    finish!

--- src/components/App.js
+++ src/components/App.js
@@ -8,11 +8,13 @@ import reducer from '../reducers';
 
 const App = () => {
    const APP_KEY = 'appWithRedux';
-   const appState = localStorage.getItem(APP_KEY)
-   const initialState = appState ? JSON.parse(appState) : { events: [], operationLogs: [] };
+   const appState = localStorage.getItem(APP_KEY);
+   const initialState = appState
+      ? JSON.parse(appState)
+      : { events: [], operationLogs: [] };
    const [state, dispatch] = useReducer(reducer, initialState);
    useEffect(() => {
-      localStorage.setItem(APP_KEY, JSON.stringify(state))
+      localStorage.setItem(APP_KEY, JSON.stringify(state));
    }, [state]);
    return (
       <AppContext.Provider value={{ state, dispatch }}>
